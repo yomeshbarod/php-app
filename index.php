bd025fada1c020693244c0f1913a65e487e86eac
yomeshbarod <barodyomesh@gmail.com>
2025-06-12 15:38:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-$host = getenv('DB_HOST');
+$env = parse_ini_file(__DIR__ . '/.env');
+
+$host = $env['DB_HOST'];
 $db   = 'bookstore';
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+$user = $env['DB_USER'];
+$pass = $env['DB_PASS'];
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -25,4 +27,3 @@ try {
     echo "<h3>❌ Database connection failed: " . $e->getMessage() . "</h3>";
 }
 ?>
-
